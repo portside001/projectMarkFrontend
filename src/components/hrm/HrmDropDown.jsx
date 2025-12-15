9cdbd687feb0d8a0a43fd5e5a949278abcd4e1de
Rohith16-45 <rohithyuvasoft469@gmail.com>
2025-12-15 16:28:56 +0530

    fixed className

--- src/components/hrm/HrmDropDown.jsx
+++ src/components/hrm/HrmDropDown.jsx
@@ -9,7 +9,7 @@ const HrmDropDown = () => {
     >
       <svg
         aria-hidden="true"
-        className="flex-shrink-0 w-6 h-6 text-gray-400 transition duration-75 group-hover:text-gray-900 dark:text-gray-400 dark:group-hover:text-white"
+        className="shrink-0 w-6 h-6 text-gray-400 transition duration-75 group-hover:text-gray-900 dark:text-gray-400 dark:group-hover:text-white"
         fill="currentColor"
         viewBox="0 0 20 20"
         xmlns="http://www.w3.org/2000/svg"
